e1f0b2fe282d59c300b337fd9732b48cb5a16ffd
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-09-22 17:10:45 +0530

    hello from gin

---
+++ 002-hello-gin/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello from %v", "Gin")
+	})
+	r.Run(":3000")
+}
