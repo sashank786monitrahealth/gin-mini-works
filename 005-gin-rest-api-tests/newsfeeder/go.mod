de1d8eeffc5f9d0fe8f28acb73081bfdc40f038f
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-11-08 19:52:02 +0530

    configured httpd

--- 005-gin-rest-api-tests/newsfeeder/go.mod
+++ 005-gin-rest-api-tests/newsfeeder/go.mod
@@ -1,3 +1,5 @@
 module newsfeeder
 
 go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ 005-gin-rest-api-tests/newsfeeder/httpd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"newsfeeder/httpd/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", handler.PingGet)
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
