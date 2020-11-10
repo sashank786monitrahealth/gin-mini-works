7263e89d909d2dc5036ee50c1c0657bb64a9e6f5
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-11-10 08:34:40 +0530

    added testing

--- 005-gin-rest-api-tests/newsfeeder/platform/newsfeed/newsfeed_test.go
+++ 005-gin-rest-api-tests/newsfeeder/platform/newsfeed/newsfeed_test.go
@@ -3,7 +3,12 @@ package newsfeed
 import "testing"
 
 func TestAdd(t *testing.T) {
+	feed := New()
+	feed.Add(Item{})
 
+	if len(feed.Items) == 0 {
+		t.Errorf("Item was not added.")
+	}
 }
 
 func TestGetAll(t *testing.T) {
